36cfcb19d8684469e3ebbc383ad91fa5809445f1
admin <admin@admins-MacBook-Air.local>
2016-08-10 16:13:39 +0530

    add metadat.rb

--- cookbooks/zookeeper/metadata.rb
+++ cookbooks/zookeeper/metadata.rb
@@ -1,7 +1,9 @@
 name             'zookeeper'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'tribu'
+maintainer_email 'tribuvan@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+supports         "CentOS 7"
+
